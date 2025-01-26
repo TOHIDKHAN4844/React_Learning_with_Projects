587611b48b8d0cf72df384303ab5c514a02996d3
Tohid Khan <tohid3707@gmail.com>
2025-01-26 17:08:23 +0530

    Lecture01

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,16 @@
+import Learn from './learn'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Learn />
+     <h1>Learning React </h1>
+    
+    </>
+   
+  )
+}
+
+export default App
---
+++ 01viteReact/src/learn.jsx
@@ -0,0 +1,7 @@
+function Learn (){
+return (
+    <h1>Time to Learn React</h1>
+)
+}
+
+export default Learn
