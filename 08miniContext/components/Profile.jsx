67510ad40001cb1d9f1c1a14dd6b10034915aece
Tohid Khan <tohid3707@gmail.com>
2025-02-03 00:16:49 +0530

    Context_API_Project

---
+++ 08miniContext/components/Profile.jsx
@@ -0,0 +1,17 @@
+import  { useContext } from 'react'
+import UserContext from '../src/context/UserContext'
+
+function Profile() {
+
+const {user} = useContext(UserContext)
+
+if(!user) return <div>Please Login</div>
+
+  return (
+    <div>
+      Welcome {user.username}
+    </div>
+  )
+}
+
+export default Profile
